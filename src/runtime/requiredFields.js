2a8df763cb6ef4ed568ba175c3bfabb91de637dd
BuffaloNYChris <buffalonychris@gmail.com>
2026-02-26 19:57:10 -0500

    PR1: add call session store, deterministic state machine, transition audit logging, and TTL janitor

---
+++ src/runtime/requiredFields.js
@@ -0,0 +1,12 @@
+module.exports = {
+  CALL_STARTED: ['callerName'],
+  IDENTITY_CHECKED: ['identityVerified'],
+  ADDRESS_CONFIRMED: ['serviceAddress'],
+  PROBLEM_CAPTURED: ['problemDescription'],
+  SCHEDULING: ['preferredTimeWindow'],
+  BOOKED: ['bookingReference'],
+  CONFIRMED_SMS_SENT: ['smsConfirmationId'],
+  LOGGED_TO_HUBSPOT: ['hubspotRecordId'],
+  ESCALATED: ['escalationReason'],
+  CALL_ENDED: []
+};
